09616e92bf05e9dc203d8b032772c132e7845607
bgw <bgwbgw1001@gmail.com>
2023-04-16 13:41:57 +0900

    commit

---
+++ lib/Intro.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    var mHeight = MediaQuery.of(context).size.height;
+    var mWidth = MediaQuery.of(context).size.width;
+    var fSize = mWidth > 600 ? 24.0 : 16.0;
+    var date = DateTime.now();
+    var toDay = DateFormat('yyyy-MM-dd').format(date);
+
+    return
+      Container(
+          color: Color(0xfffefae0),
+
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: mHeight*0.5),
+                decoration: BoxDecoration( borderRadius: BorderRadius.circular(10),color: Color(0xfffefae0),),
+              ),
+              SizedBox(
+                width: mWidth*0.4,
+                height: mHeight*0.1,
+                child: TextButton(
+                    child: Text('I\'M FREE'),
+                    style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0))
+                        ,side: BorderSide(width: 1.0, color: Colors.blue)
+                    ),
+                    onPressed: () {
+                    }
+                ),
+              ),
+              SizedBox(
+                width: mWidth*0.4,
+                height: mHeight*0.1,
+                child: TextButton(
+                    child: Text(toDay,style: TextStyle(fontSize: fSize))
+                    ,onPressed: (){
+                }),
+              )
+            ],
+          )
+      );
+  }
+}
